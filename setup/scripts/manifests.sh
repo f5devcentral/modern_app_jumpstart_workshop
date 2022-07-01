4f5f47443e1b577df8a4bf570454278d5677219a
Cody Green <cody@codygreen.com>
2022-07-01 16:28:24 -0500

    remove manifests; add nginx-ingress

--- setup/scripts/manifests.sh
+++ setup/scripts/manifests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # copy manifests to K3s manifest folder
-cp /root/modern_app_jumpstart_workshop/setup/manifests/argocd.yml /var/lib/rancher/k3s/server/manifests/
+cp /root/modern_app_jumpstart_workshop/setup/manifests/* /var/lib/rancher/k3s/server/manifests/
