ec1ac3ec3769ebff9e28f67aab4a8058ecf266b4
Cody Green <cody@codygreen.com>
2022-07-02 14:53:32 -0500

    fix script execution issue

--- setup/scripts/nginx.sh
+++ setup/scripts/nginx.sh
@@ -2,7 +2,15 @@
 PWD=/root/modern_app_jumpstart_workshop/setup
 
 # copy config
+echo "copying NGINX config"
 cp $PWD/config/default.conf /etc/nginx/conf.d/default.conf
 
+# test NGINX config
+echo "testing NGINX config"
+nginx -t
+
 # restart nginx
+echo "restarting NGINX"
 systemctl restart nginx
+
+nginx -T
